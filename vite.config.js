0d8092f1ec7bd8d182657c2f7729fe0f4dd5ea53
Abdulrahman_Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-16 14:12:43 +0400

    install laravel-frontend-presets/tall package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,61 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({command, mode, ssrBuild}) => {
+    if (command === 'serve') {
+        return {
+            server: {
+                host: '0.0.0.0',
+                hmr: {
+                    // host: 'localhost',
+                    host: 'tall-stack.test',
+                },
+                watch: {
+                    usePolling: true,
+                },
+            },
+            build: {
+                manifest: true,
+                outDir: 'public/build',
+                rollupOptions: {
+                    input: 'resources/js/app.js',
+                },
+                target: ['esnext']
+            },
+            plugins: [
+                laravel({
+                    input: ['resources/js/app.js'],
+                    refresh: true,
+                }),
+            ]
+        }
+    } else {
+        return {
+            server: {
+                host: '0.0.0.0',
+                hmr: {
+                    // host: 'localhost',
+                    host: 'tall-stack.test',
+                },
+                watch: {
+                    usePolling: true,
+                },
+            },
+            build: {
+                manifest: true,
+                outDir: 'public/build',
+                rollupOptions: {
+                    input: 'resources/js/app.js',
+                },
+                target: ['esnext']
+            },
+            plugins: [
+                laravel({
+                    input: ['resources/js/app.js'],
+                    refresh: true,
+                }),
+            ]
+        }
+    }
+
 });
